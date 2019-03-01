5643f5f4bccab0659b8d052aa4687565d707983e
Andrew-M-C <laplacezhang@126.com>
2019-03-01 16:18:59 +0800

    Add stdout debug tool

--- log/debug.go
+++ log/debug.go
@@ -6,9 +6,10 @@ import (
 	"fmt"
 )
 
-func Debug(format string, v ...interface{}) string {
+func Debug(format string, v ...interface{}) {
 	datetime := getTimeStr()
 	file, line, function := getCallerInfo(0);
 	text := fmt.Sprintf(format, v...)
 	fmt.Println(datetime + ", " + file + ", Line " + strconv.Itoa(line) + ", " + function + "() - " + text)
+	return
 }
